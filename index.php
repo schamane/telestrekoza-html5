7666ffe460d09794ce1f90e2d3717875891296fb
Matt Vears <matt.vears@gmail.com>
2011-04-21 17:43:22 -0400

    Using wordpress' bloginfo function to populate meta tags and paths

--- index.php
+++ index.php
@@ -1,36 +1,39 @@
 <!doctype html>
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js ie6" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if lt IE 7]> <html class="no-js ie6" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
 
   <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
        Remove this if you use the .htaccess -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <title></title>
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
+  <meta name="description" content="<?php bloginfo('description'); ?>">
+  <meta name="author" content="<?php bloginfo('admin_email'); ?>">
 
   <!-- Mobile viewport optimized: j.mp/bplateviewport -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- Place favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-  <link rel="shortcut icon" href="/favicon.ico">
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
+  <link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/apple-touch-icon.png">
 
 
-  <!-- CSS: implied media="all" -->
-  <link rel="stylesheet" href="css/style.css">
-
+  <!-- Keeping the HTML5 Boilerplate directory structure, the root level style.css is here to satisfy wordpress only. -->
+  <!-- CSS: implied media="all" -->  
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
+	
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+  
   <!-- Uncomment if you are specifically targeting less enabled mobile browsers
-  <link rel="stylesheet" media="handheld" href="css/handheld.css">  -->
+  <link rel="stylesheet" media="handheld" href="<?php bloginfo('template_url'); ?>/css/handheld.css">  -->
 
   <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
-  <script src="js/libs/modernizr-1.7.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/libs/modernizr-1.7.min.js"></script>
 
 </head>
 
@@ -38,10 +41,12 @@
 
   <div id="container">
     <header>
-
+		<?php bloginfo('name'); ?>
+		<?php bloginfo('description'); ?>
     </header>
     <div id="main" role="main">
-
+		
+		
     </div>
     <footer>
 
@@ -53,12 +58,12 @@
 
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.js"></script>
-  <script>window.jQuery || document.write("<script src='js/libs/jquery-1.5.2.min.js'>\x3C/script>")</script>
+  <script>window.jQuery || document.write("<script src='<?php bloginfo('template_url'); ?>/js/libs/jquery-1.5.2.min.js'>\x3C/script>")</script>
 
 
   <!-- scripts concatenated and minified via ant build script-->
-  <script src="js/plugins.js"></script>
-  <script src="js/script.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
   <!-- end scripts-->
 
 
