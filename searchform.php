1ff65506b4bcfed88459c4ff6f74d56e417ecac5
Matt Vears <matt.vears@gmail.com>
2011-04-29 17:20:49 -0400

    searchform

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div id='search-form'>
+	<label class="screen-reader-text" for="s">Search for:</label>
+        <input type="search" value="" name="s" id="s" />
+        <input type="submit" id="searchsubmit" value="Search" />
+    </div>
+</form>
+
