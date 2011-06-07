81a83d36556fd874b686733d04d7ab1617bf0b74
Matt Vears <matt.vears@gmail.com>
2011-06-06 22:38:43 -0400

    addresses https://github.com/mattvears/html5-wordpress-boilerplate/issues/2

--- sidebar.php
+++ sidebar.php
@@ -5,37 +5,40 @@
 		</section>
 	</aside>
 
-	<aside>
-		<section id='integration' role='list'>
-			<?php $options = get_option('theme_options');
-				$twitterId =  $options['twitter_id'];
-				$facebookId =  $options['facebook_id'];
-				$googleId =  $options['google_id'];
-			?>
+	<?php $options = get_option('theme_options');
+        $twitterId =  $options['twitter_id'];
+        $facebookId =  $options['facebook_id'];
+        $googleId =  $options['google_id'];
 
-			<h3>Other Sites</h3>
-			<ul>
-			<?	if (!is_null($twitterId) && !empty($twitterId)) { ?>
-				<li>
-					<img src="<?php bloginfo('template_url'); ?>/img/site-icons/twitter.png" />
-					<a href="http://twitter.com/<?php echo $twitterId; ?>" />Twitter</a>
-				</li>
-			<?php } ?>
-
-			<?	if (!is_null($facebookId) && !empty($facebookId)) { ?>
-				<li>
-					<img src="<?php bloginfo('template_url'); ?>/img/site-icons/facebook.png" />
-					<a href="http://facebook.com/<?php echo $facebookId; ?>" />Facebook</a>
-				</li>
-			<?php } ?>
-
-			<?	if (!is_null($googleId) && !empty($googleId)) { ?>
-				<li>
-					<img src="<?php bloginfo('template_url'); ?>/img/site-icons/google.png" />
-					<a href="http://profiles.google.com/<?php echo $googleId; ?>" />Google profile</a>
-				</li>
-			<?php } ?>
-
-			</ul>
-		</section>
-	</aside>
+	    if (!empty($twitterId) || !empty($facebookId) || !empty($googleId)) { 
+	?>
+	    <aside>
+            <section id='integration' role='list'>
+    
+                <h3>Other Sites</h3>
+                <ul>
+                <?	if (!is_null($twitterId) && !empty($twitterId)) { ?>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/site-icons/twitter.png" />
+                        <a href="http://twitter.com/<?php echo $twitterId; ?>" />Twitter</a>
+                    </li>
+                <?php } ?>
+    
+                <?	if (!is_null($facebookId) && !empty($facebookId)) { ?>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/site-icons/facebook.png" />
+                        <a href="http://facebook.com/<?php echo $facebookId; ?>" />Facebook</a>
+                    </li>
+                <?php } ?>
+    
+                <?	if (!is_null($googleId) && !empty($googleId)) { ?>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/site-icons/google.png" />
+                        <a href="http://profiles.google.com/<?php echo $googleId; ?>" />Google profile</a>
+                    </li>
+                <?php } ?>
+    
+                </ul>
+            </section>
+        </aside>
+    <?php } ?>
